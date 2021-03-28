4fd79380167fc42b1fa246508d39f371c597c885
Allan Cristian <Allan.cristian@outlook.com.br>
2021-03-27 21:29:02 -0300

    create route get statement

--- src/index.js
+++ src/index.js
@@ -16,6 +16,16 @@ const customers = [];
 
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
+
+  const customerAlreadyExists = customers.some(
+    (customer) => 
+      customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: "Customer already exists!"});
+  }
+
   const id = uuidv4();
 
   customers.push({
@@ -26,8 +36,16 @@ app.post("/account", (request, response) => {
   });
 
   return response.status(201).send();
-
-
 });
 
+app.get('/statement/:cpf', (request, response) => {
+    const { cpf } = request.params;
+
+    const customer = customers.find(
+      customer => 
+        customer.cpf === cpf
+    );
+
+    return response.json(customer.statement);
+});
 app.listen(3333);
